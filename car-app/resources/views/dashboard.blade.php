967b2933c2b868351766d5208ac20fa46974d1c9
Andrej Nankov <andrejnankov@gmail.com>
2024-04-20 22:27:46 +0200

    Upd dashboard

--- car-app/resources/views/dashboard.blade.php
+++ car-app/resources/views/dashboard.blade.php
@@ -1,16 +1,18 @@
 <x-app-layout>
     <main>
-        <livewire:vehicle />
+        <div class="grid lg:grid-cols-2 pb-4 gap-4 md:grid-cols-2 sm:grid-cols-1 grid-cols-1 lg:mb-4 md:mb-4">
+            <livewire:vehicle />
 
-        <div class="my-4">
-            <livewire:base-information-table />
+            <div class="my-4 sm:pt-2 pt-0">
+                <livewire:base-information-table />
+            </div>
         </div>
 
-        <div class="container">
+        <div class="container pb-4">
             <livewire:needed-services-table />
         </div>
 
-        <div class="container">
+        <div class="container my-4">
             <livewire:service-history-table />
         </div>
     </main>
