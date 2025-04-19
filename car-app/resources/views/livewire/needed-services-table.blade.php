70babfdcd0d9f91ebc15a6b87c5c4cc2f8f23bef
nanorocks <andrejnankov@gmail.com>
2025-04-20 01:10:12 +0200

    Add icons for actions

--- car-app/resources/views/livewire/needed-services-table.blade.php
+++ car-app/resources/views/livewire/needed-services-table.blade.php
@@ -4,13 +4,15 @@
             <div class="btn btn-sm small">Needed services</div>
             @if ($isEditMode)
                 <div>
-                    <a href="{{ route('dashboard') }}" class="btn btn-sm btn-link px-3">Go back</a>
+                    <a href="{{ route('vehicles.show', ['id' => $vehicleId]) }}" class="btn btn-sm btn-link px-3">Go
+                        back</a>
                     <button class="btn btn-sm btn-link px-0" onclick="window.location.reload()">Create new</button>
                 </div>
             @endif
         </div>
         @if (!$isEditMode)
-            <a href="{{ route('needed.service.manager', ['id' => $vehicleId]) }}" class="btn btn-sm btn-primary small">Content
+            <a href="{{ route('needed.service.manager', ['id' => $vehicleId]) }}"
+                class="btn btn-sm btn-primary small">Content
                 Manager</a>
         @endif
     </div>
@@ -64,12 +66,28 @@
                         <td>{{ $item->next_service_due }}</td>
                         @if ($isEditMode)
                             <th>
-                                <div class="d-flex">
-                                    <button class="mx-1 small text-success btn btn-link btn-sm"
-                                        wire:click="edit({{ $item->id }})">Edit</button>
-                                    <button button class="mx-1 small text-error btn btn-link btn-sm"
+                                <div class="d-flex gap-2">
+                                    <button class="small text-success" wire:click="edit({{ $item->id }})"> <svg
+                                            xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24"
+                                            height="24" class="main-grid-item-icon" fill="none"
+                                            stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
+                                            stroke-width="2">
+                                            <path d="M11 4H4a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-7" />
+                                            <path d="M18.5 2.5a2.121 2.121 0 0 1 3 3L12 15l-4 1 1-4 9.5-9.5z" />
+                                        </svg></button>
+                                    <button button class="small text-error"
                                         wire:click="delete({{ $item->id }})"
-                                        wire:confirm="Are you sure you want to delete this item?">Delete</button>
+                                        wire:confirm="Are you sure you want to delete this item?"><svg
+                                            xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24"
+                                            height="24" class="main-grid-item-icon" fill="none"
+                                            stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
+                                            stroke-width="2">
+                                            <polyline points="3 6 5 6 21 6" />
+                                            <path
+                                                d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6m3 0V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2" />
+                                            <line x1="10" x2="10" y1="11" y2="17" />
+                                            <line x1="14" x2="14" y1="11" y2="17" />
+                                        </svg></button>
                                 </div>
                             </th>
                         @endif
